22833d60bbfb1c9794c948654dc8df719954c8b7
Medved Dima <dimam@assuta.co.il>
2024-11-07 23:25:48 +0200

    OCRImages function

---
+++ src/lib/OCRImages.ts
@@ -0,0 +1,26 @@
+import { createWorker, createScheduler } from 'tesseract.js';
+
+const OCRImages = async (imagesData: string[], options?: any) => {
+  if (!imagesData) return;
+
+  console.log('imagesData', imagesData);
+  const worker = await createWorker(['eng', 'heb'], 1, {
+    logger: (m) => {
+      console.log('m', m);
+    },
+  });
+
+  const promises = imagesData.map(async (image) => {
+    const {
+      data: { text },
+    } = await worker.recognize(image);
+    return text;
+  });
+
+  const texts = await Promise.all(promises);
+  return texts.reduce((acc, text, index) => {
+    return { ...acc, [index + 1]: text };
+  }, {});
+};
+
+export default OCRImages;
