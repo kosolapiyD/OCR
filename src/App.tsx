72706aa653a167aa4ae4c05c8f78c2d2ead0d222
Medved Dima <dimam@assuta.co.il>
2024-11-08 00:00:48 +0200

    image recognition response ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import './App.css';
+import OCRImages from './lib/OCRImages';
 import HomeScreen from './screens/HomeScreen';
 
 function App() {
   const handleFileSelect = (file: File) => {
-    console.log('file', file?.type);
+    if (!file) return;
+
+    const fileType = file?.type;
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      const fileDataUri: any = reader.result;
+      console.log('fileDataUri => ', fileDataUri);
+      if (fileType?.includes('image')) {
+        const ocrResponse = await OCRImages([fileDataUri]);
+        console.log('ocrResponse', ocrResponse);
+      } else if (fileType?.includes('pdf')) {
+        // console.log('file', file);
+      } else {
+        console.log('Invalid file type');
+      }
+    };
+    reader.readAsDataURL(file);
   };
-  return (
-    <>
-      <HomeScreen onFileAccepted={handleFileSelect} />
-    </>
-  );
+  return <HomeScreen onFileAccepted={handleFileSelect} />;
 }
 
 export default App;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,20 @@
 import { AiFillInfoCircle } from 'react-icons/ai';
 import FileUpload from '../components/file-upload/FileUpload';
 
+import './HomeScreen.css';
+
 const HomeScreen = ({ onFileAccepted }: any) => {
   return (
-    <div>
-      <h2>OCR App with Tesseract.js</h2>
-      <FileUpload onFileAccepted={onFileAccepted} />
+    <div className='homeContainer'>
       <div className='info'>
-        <AiFillInfoCircle size={20} />
-        <p>
-          This app uses Tesseract.js to extract text from images. Drop an image
-          file to get started.
-        </p>
+        <h2>OCR App with Tesseract.js</h2>
+        <div className='paragraph'>
+          <p>This app uses Tesseract.js to extract text from images.</p>
+          <AiFillInfoCircle size={20} />
+        </div>
       </div>
+
+      <FileUpload onFileAccepted={onFileAccepted} />
     </div>
   );
 };
